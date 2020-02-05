810daedfb8ca3d0ad69d0d8949829e0c7af9c4e1
GitHub Action <action@github.com>
2020-02-05 17:49:45 +0000

    chore: Deploy website

--- docs/components/Visualizer.js
+++ docs/components/Visualizer.js
@@ -1,4 +1,4 @@
-import{createVNode}from"/web_modules/inferno.js";/*
+import{createVNode}from"/inferno-snowpack-ts-demo/web_modules/inferno.js";/*
  * This is example of Inferno functional component
  * Functional components provide great performance but does not have state
  */export function Visualizer(a){return createVNode(1,"div","visualizer",a.value,0)}
